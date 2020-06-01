52baee64808c62b216689b64a7558d5a5be9b7e6
Ted <thm2118@columbia.edu>
2020-06-01 14:45:13 +0100

    added tensorboard-like smoothing

--- ops.py
+++ ops.py
@@ -2,7 +2,26 @@ import numpy as np
 
 def running_mean(x, N):
     """
-    compute the running mean with a window size of N
+    compute the running mean 
+    args:
+        x: 1D series of scalar data 
+        N: smoothing window size 
     """ 
     cumsum = np.cumsum(np.insert(x, 0, 0)) 
     return (cumsum[N:] - cumsum[:-N]) / float(N)
+
+def smooth(scalars, weight=0.75):  
+    """
+    perform exponential smoothing of a series
+    args:
+        scalars: 1D list of scalars 
+        weight: smoothing factor, between 0 and 1 (higher = greater smoothing)
+    """
+    last = scalars[0]  # First value in the plot (first timestep)
+    smoothed = list()
+    for point in scalars:
+        smoothed_val = last * weight + (1 - weight) * point  # Calculate smoothed value
+        smoothed.append(smoothed_val)                        # Save it
+        last = smoothed_val                                  # Anchor the last smoothed value
+
+    return smoothed
